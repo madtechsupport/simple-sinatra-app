956b7471456ad8cd9e9c021b43dec98add68fef5
root <root@localhost.localdomain>
2014-05-15 07:02:27 +0100

    Getting there with the modules path.

--- puppet/manifests/modules.pp
+++ puppet/manifests/modules.pp
@@ -1,12 +1,9 @@
 module { 'puppetlabs/apache': 
 	ensure => present, 
-	modulepath => $basemodulepath,
 }
 module { 'puppetlabs/firewall': 
 	ensure => present, 
-	modulepath => $basemodulepath,
 }
 module { 'puppetlabs/ruby': 
 	ensure => present, 
-	modulepath => $basemodulepath,
 }
