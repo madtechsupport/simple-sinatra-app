9e4b10804bc3a5be37c83fd70a42e12cc69a1e65
madtechsupport <warren@madtechsupport.com>
2014-05-19 12:44:40 +1000

    Testing setup support for Debian 7.

---
+++ README.md
@@ -0,0 +1,24 @@
+Instructions
+============
+
+This script has been tested with Centos 6.5 (i386) minimal iso and Debian...
+
+Centos 6.5 (i386) minimal iso
+-----------------------------
+(These steps should also work for RHEL 6).
+1. Log in as `root`
+2. Install git, `yum install git -y`
+3. Change directory to the location where you want to clone the repository.
+4. Clone the repository: 'git clone https://github.com/tnh/simple-sinatra-app.git`
+5. There should now be a new `simple-sinatra-appr` directory, change to that directory.
+6. Run `setup.sh` to install and run the application using `/var/www/simple-sinatra-app` as the "document root", or run `setup.sh` followed by the full path of
+an alternative location e.g. `setup.sh /var/ruby/simple-sinatra-app`.
+7. Wait for the script to complete and then test by pointing your browser to port 80 of the external ip address of the host.
+
+### Header 3
+
+> This is a blockquote.
+> 
+> This is the second paragraph in the blockquote.
+>
+> ## This is an H2 in a blockquote
--- setup.sh
+++ setup.sh
@@ -8,14 +8,27 @@ set -e
 # Define environment variables.
 
 setuppath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-installpath="/var/www/simple-sinatra-app"
+installpath="${1:=/var/www/simple-sinatra-app}"
 gitrepository="https://github.com/tnh/simple-sinatra-app"
 selinuxcreatepolicy="${setuppath}/selinux_create_policy.sh"
 exfactdir="/etc/facter/facts.d"
 exfactfile="facts.txt"
 platform=$(uname -i)
+release=$(cat /etc/*-release)
 repo_url_el_6="https://yum.puppetlabs.com/el/6/products/${platform}/puppetlabs-release-6-7.noarch.rpm"
-repo_url_apt="https://apt.puppetlabs.com/puppetlabs-release-${code_name}.deb"
+repo_url_apt="https://apt.puppetlabs.com/puppetlabs-release-${codename}.deb"
+
+# Need to be root.
+if [[ ${EUID} -ne 0 ]]; then
+  echo "This script must be run as root." 1>&2
+  exit 1
+fi
+
+# Don't do anything if puppet is already installed.
+if type -P puppet >/dev/null 2>&1; then
+  echo "Puppet is already installed."
+  exit 1
+fi
 
 # Check to see if the submodule is present.
 if [[ ! -f ${setuppath}/modules/passenger/README.md ]]; then
@@ -29,46 +42,38 @@ if [[ ! -f ${setuppath}/modules/passenger/README.md ]]; then
   fi
 fi
 
-# Work out the Linux distribution and code name.
-distribution() { 
-if grep "^CentOS release 6" /etc/centos-release >/dev/null 2>&1; then
-  # This is Centos 6.
-  if [[ ${platform} != "i386" && ${platform} != "x86_64" ]]; then
-    echo "Puppet Labs Enterprise Linux 6 yum repositories available for i386 and x86_64 hardware platforms only."
-    exit 1
-  else
+# Work out the Linux distribution and install puppet.
+case ${release} in
+  "release 6")
+    # This is Centos or Redhat 6.
+    # Check the architecture is supported.
+    if [[ ${platform} != "i386" && ${platform} != "x86_64" ]]; then
+      echo "Puppet Labs Enterprise Linux 6 yum repositories available for i386 and x86_64 hardware platforms only."
+      exit 1
+    fi
+    # Install puppet
+    printf "Installing puppet now.\n"
     rpm_path=$(mktemp -d)/puppetlabs_release.rpm
     repo_url=${repo_url_el_6}
-  fi
-else
-  printf "I don't know this operating system.\n"
-  exit 1
-fi 
-}
-
-# main()
-# Install puppet labs repo
-
-if [[ ${EUID} -ne 0 ]]; then
-  echo "This script must be run as root." 1>&2
-  exit 1
-fi
-
-if type -P puppet >/dev/null 2>&1; then
-  echo "Puppet is already installed."
-  exit 1
-fi
-
-distribution
-printf "Configuring PuppetLabs repo...\n"
-curl -L -o "${rpm_path}" "${repo_url}" 2>/dev/null
-yum localinstall "${rpm_path}" -y >/dev/null
-
-# Install Puppet...
-echo "Installing puppet"
-yum install -y puppet > /dev/null
-
-echo "Puppet installed!"
+    curl -L -o "${rpm_path}" "${repo_url}" 2>/dev/null
+    yum localinstall "${rpm_path}" -y >/dev/null
+    yum install -y puppet > /dev/null
+  ;;
+  "wheezy")
+    # This is Debian 7.
+    codename="wheezy"
+    # Install puppet.
+    printf "Installing puppet now.]n"
+    deb_path=$(mktemp -d)/puppetlabs_release.deb
+    curl -L -o "${deb_path}" "${repo_url_apt}" 2>/dev/null
+    dpkg -i ${deb_path}
+    apt-get update 
+  ;;
+  *)
+    printf "Sorry, I don't know this operating system.\n"
+    exit 1
+  ;;
+esac 
 
 # Next save some things as external facts.
 if [[ -d ${exfactdir} ]]; then
@@ -96,3 +101,6 @@ puppet module install puppetlabs/vcsrepo
 
 # Run puppet.
 puppet apply ${setuppath}/manifests/site.pp --modulepath '$basemodulepath':${setuppath}/modules/
+
+# We made it!
+printf "Installation completed. Please test by pointing your browser to port 80 of the host\'s IP address and checking for the welcome message."
